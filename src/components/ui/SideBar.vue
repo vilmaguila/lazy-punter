6f1b6eaa6202d1b0c2816eacff71746138837f44
Ville Mäkilä <ville.makila@protonmail.com>
2021-04-25 20:11:09 +0300

    finished unstyled about related components

--- src/components/ui/SideBar.vue
+++ src/components/ui/SideBar.vue
@@ -1,9 +1,33 @@
 <template>
-    <router-view v-for="link in links" :key=link.id></router-view>
+  <div class="container">
+    <router-link :to="{ name: 'Address' }">Address</router-link> |
+    <router-link :to="{ name: 'Ethics' }">Ethics</router-link> |
+    <a>dummy</a>
+  </div>
 </template>
 
 <script>
 export default {
-    
-}
+  name: "SideBar",
+  data() {
+    return {
+      links: [
+        {
+          id: "addr",
+          link: "address",
+        },
+        {
+          id: "eth",
+          link: "ethics",
+        },
+      ],
+    };
+  },
+};
 </script>
+
+<style scoped>
+.container {
+    display: inline-block;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <navigation-bar></navigation-bar>
+    <side-bar></side-bar>
     <router-view></router-view>
     <router-view name="mission"></router-view>
     <router-view name="vision"></router-view>
@@ -9,13 +9,19 @@
 </template>
 
 <script>
-import NavigationBar from '@/components/ui/NavigationBar.vue'
+import SideBar from '@/components/ui/SideBar.vue'
 
 export default {
   name: 'About',
   components: {
-    NavigationBar,
+    SideBar,
   }
   
 }
 </script>
+
+<style scoped>
+.about {
+  display: grid
+}
+</style>
