4751aed1ec0166284c4e1cb31c700ac3eaf42af3
Ville Mäkilä <ville.makila@protonmail.com>
2021-04-28 21:53:19 +0300

    styled the-footer and provided dummy content

--- src/components/layout/TheFooter.vue
+++ src/components/layout/TheFooter.vue
@@ -1,5 +1,19 @@
 <template>
-  <footer>This represents a footer</footer>
+  <footer class="flex flex-row justify-evenly">
+    <div class="">
+      <h2>Linkit</h2>
+      <a href="https://www.google.com">Google</a>
+    </div>
+    <div class="">
+      <h2>Yhteystiedot</h2>
+      <p>Pillipiiparinpolku 1</p>
+    </div>
+    <div class="">
+      <h2>Tietosuoja</h2>
+      <a href="link">linkki</a> <br />
+      <a href="link">linkki</a>
+    </div>
+  </footer>
 </template>
 
 <script>
@@ -7,3 +21,9 @@ export default {
   name: "TheFooter",
 };
 </script>
+
+<style lang="postcss" scoped>
+footer {
+  @apply py-2 px-4 font-semibold bg-gray-500 font-sans; 
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,22 @@
 <template>
-  <div class="home">
-    <navigation-bar></navigation-bar>
+  <div class="grid grid-cols-4 gap-4">
+    <navigation-bar class="col-span-4"></navigation-bar>
     <h1>Home</h1>
     <hello-world msg="Message as a prop"></hello-world>
-    <the-footer></the-footer>
+    <hello-world></hello-world>
+    <the-footer class="col-span-4"></the-footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld.vue'
-import NavigationBar from '../components/ui/NavigationBar.vue'
+import HelloWorld from "../components/HelloWorld.vue";
+import NavigationBar from "../components/ui/NavigationBar.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     HelloWorld,
     NavigationBar,
-  }
+  },
 };
 </script>
